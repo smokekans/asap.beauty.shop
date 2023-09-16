1ccb3dacf8ed2f89a8ce167e8eb74e24a7828f60
Dmytro Savchenko <smokekans@gmail.com>
2023-09-16 09:30:55 +0300

    update product

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,31 +1,16 @@
 import "swiper/swiper-bundle.min.css";
 import { Box, List, ListItem, Typography } from "@mui/material";
-import { Swiper, SwiperSlide } from "swiper/react";
-
 export default function Product({ data }) {
   return (
     <>
       <Box>
-        <Swiper
-          spaceBetween={50}
-          slidesPerView={3}
-          navigation
-          pagination={{ clickable: true }}
-          scrollbar={{ draggable: true }}
-          onSwiper={(swiper) => console.log(swiper)}
-          onSlideChange={() => console.log("slide change")}
-        >
-          {data?.image?.map((item, index) => (
-            <SwiperSlide key={index}>
-              <Box
-                component="img"
-                sx={{ width: 500 }}
-                alt="product"
-                src={item?.image}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        <Box
+          component="img"
+          sx={{ width: 500 }}
+          alt="product"
+          src={data?.image}
+        />
+
         <Typography variant="h3">
           {data?.brand} {data?.line} {data?.product} {data?.name}
         </Typography>
